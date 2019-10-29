b87badec45c932ad8584d2df86c6ba264e564eed
CarnunMP <carnun@hotmail.co.uk>
2019-10-29 13:35:38 +0000

    POST /api/posts done

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const db = require('./data/db');
 
 const server = express();
 server.use(express.json());
 
 server.get('/', (req, res) => {
   res.json('Hello World!');
+});
+
+
+server.post('/api/posts', (req, res) => {
+  const { title, contents } = req.body;
+  if (!title || !contents) {
+    res.status(400).json({
+      success: false,
+      message: 'Please provide a title and contents.',
+    });
+  } else {
+    db.insert({ title, contents })
+      .then((data) => {
+        res.status(201).json({
+          success: true,
+          id: data.id,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          err,
+          message: 'There was an error while saving the post to the database',
+        });
+      });
+  }
 });
 
 module.exports = server;
